d32717d92d2d23d32574ea46043620dfce92c8a5
caaano <wlsrl0515@naver.com>
2023-12-13 10:21:04 +0900

    Repository

---
+++ src/main/java/com/mysite/medium/WriteRepository.java
@@ -0,0 +1,6 @@
+package com.mysite.medium;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WriteRepository extends JpaRepository <Write, Integer> {
+}
--- src/test/java/com/mysite/medium/MediumApplicationTests.java
+++ src/test/java/com/mysite/medium/MediumApplicationTests.java
@@ -11,20 +11,20 @@ import java.time.LocalDateTime;
 class SbbApplicationTests {
 
     @Autowired
-    private QuestionRepository questionRepository;
+    private WriteRepository writeRepository;
 
     @Test
     void testJpa() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);  // 첫번째 질문 저장
+        Write w1 = new Write();
+        w1.setSubject("sbb가 무엇인가요?");
+        w1.setContent("sbb에 대해서 알고 싶습니다.");
+        w1.setCreateDate(LocalDateTime.now());
+        this.writeRepository.save(w1);  // 첫번째 질문 저장
 
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);  // 두번째 질문 저장
+        Write w2 = new Write();
+        w2.setSubject("스프링부트 모델 질문입니다.");
+        w2.setContent("id는 자동으로 생성되나요?");
+        w2.setCreateDate(LocalDateTime.now());
+        this.writeRepository.save(w2);  // 두번째 질문 저장
     }
 }
